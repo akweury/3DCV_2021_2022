a16de527fee94d094812d573dd0889692e1eb04f
jingyuan.sha <sha138cc>
2022-01-12 15:40:20 +0100

    ex4 solution update

--- Exercise_4/template.py
+++ Exercise_4/template.py
@@ -16,7 +16,7 @@ os.makedirs(f'./{experiment}', exist_ok=True)
 # While experimenting it is better to work with a lower resolution version of the image
 # Since the dataset is of high resolution we will work with resized version.
 # You can choose the reduction factor using the scale_factor variable.
-scale_factor = 2
+scale_factor = 1
 
 # Choose similarity metric
 # similarity_metric = 'ncc'
@@ -24,12 +24,12 @@ similarity_metric = 'ssd'
 
 # Outlier Filtering Threshold. You can test other values, too.
 # This is a parameter which you have to select carefully for each dataset
-outlier_threshold = 4
+outlier_threshold = 3
 
 # Patch Size
 # Experiment with other values like 3, 5, 7,9,11,15,13,17 and observe the result
 
-patch_width = 17
+patch_width = 7
 
 if dataset == 'kitti':
     # Minimum and maximum disparies
@@ -113,15 +113,8 @@ def ply_creator(input_3d, rgb_data=None, filename='dummy'):
             if i != input_3d.shape[0]:
                 fid.write('\n')
     fid.close()
+    print('plt file has been saved.')
     return True
-
-
-def disparity_to_depth(disparity, baseline):
-    """
-    Converts disparity to depth.
-    """
-    inv_depth = (disparity + 10e-5) / (baseline * focal_length)
-    return 1 / inv_depth
 
 
 def write_depth_to_file(depth, f_name):
@@ -168,6 +161,14 @@ def extract_pathches(img, patch_width):
     return patches
 
 
+def disparity_to_depth(disparity, baseline):
+    """
+    Converts disparity to depth.
+    """
+    inv_depth = (disparity + 10e-5) / (baseline * focal_length)
+    return 1 / inv_depth
+
+
 # **************************************** Utilitiy Functions ************************************#
 
 def depth_to_3d(depth_map, kmat):
@@ -181,6 +182,8 @@ def depth_to_3d(depth_map, kmat):
 
     2. Then scale it by its depth.
     """
+    inv_K = np.linalg.inv(kmat)
+
     xl = np.arange(0, resized_l_img.shape[1], 1)
     xl = np.reshape(xl, (1, xl.shape[0], 1))
     xl = np.repeat(xl, resized_l_img.shape[0], axis=0)
@@ -189,11 +192,21 @@ def depth_to_3d(depth_map, kmat):
     yl = np.reshape(yl, (yl.shape[0], 1, 1))
     yl = np.repeat(yl, resized_l_img.shape[1], axis=1)
 
-    z = depth_map.reshape(depth_map.shape[0], depth_map.shape[1], 1)
-    x = xl * z / focal_length
-    y = yl * z / focal_length
+    points_img_homo = np.concatenate((xl, yl, np.ones(shape=(resized_l_img.shape[0], resized_l_img.shape[1], 1))),
+                                     axis=2)
+    # points_3d = points_img_homo  @ inv_K / np.expand_dims(depth_map, axis=2)
+    # points_3d = (points_img_homo * np.expand_dims(depth_map, axis=2)) @ inv_K
 
-    points_3d = np.concatenate((x, y, z), axis=2)
+    points_3d = np.empty(resized_l_img.shape, dtype=float)
+    for i in range(resized_l_img.shape[0]):
+        for j in range(resized_l_img.shape[1]):
+            points_3d[i, j, :] = (depth_map[i, j] * inv_K @ points_img_homo[i, j, :].reshape(3, 1)).reshape(3)
+
+    # z = depth_map.reshape(depth_map.shape[0], depth_map.shape[1], 1)
+    # x = xl * z / focal_length
+    # y = yl * z / focal_length
+    # points_3d = np.concatenate((x, y, z), axis=2)
+
     return points_3d
 
 
@@ -201,14 +214,29 @@ def mask_outliers(similiarity_scores, sim_metric, threshold):
     '''
     Details are given in the exercise sheet.
     '''
-
-    raise NotImplementedError
+    scores = similiarity_scores
+    if sim_metric == 'ssd':
+        min_scores = np.min(similiarity_scores, axis=2)
+        for i in range(similiarity_scores.shape[0]):
+            for j in range(similiarity_scores.shape[1]):
+                if (similiarity_scores[i, j, :] < 1.5 * min_scores[i, j]).sum() > threshold:
+                    # reject this pixel
+                    scores[i, j, :] = -1
+    else:
+        max_scores = np.max(similiarity_scores, axis=2)
+        for i in range(similiarity_scores.shape[0]):
+            for j in range(similiarity_scores.shape[1]):
+                if (similiarity_scores[i, j, :] > 0.8 * max_scores[i, j]).sum() > threshold:
+                    # reject this pixel
+                    scores[i, j, :] = 1
+    return scores
 
 
 def ssd(feature_1, feature_2):
     '''
     Compute the sum of square difference between the input features
     '''
+
     [w, h, fw, fh, ch] = feature_1.shape
     vector_feature_1 = feature_1.reshape(w, h, fw * fh * ch)
     vector_feature_2 = feature_2.reshape(w, h, fw * fh * ch)
@@ -225,12 +253,10 @@ def ncc(feature_1, feature_2):
     vector_feature_1 = feature_1.reshape(w, h, fw * fh * ch)
     vector_feature_2 = feature_2.reshape(w, h, fw * fh * ch)
 
-    vector_feature_1_sum = np.sum(vector_feature_1, axis=-1)
-    vector_feature_2_sum = np.sum(vector_feature_2, axis=-1)
+    norm = np.linalg.norm(vector_feature_1, axis=-1) * np.linalg.norm(vector_feature_2, axis=-1)
 
-    cost = vector_feature_1_sum * vector_feature_2_sum / (
-            np.linalg.norm(vector_feature_1, axis=2) * np.linalg.norm(vector_feature_2, axis=2) + 1e-5
-    )
+    cost = np.sum(vector_feature_1 * vector_feature_2, axis=-1) / (norm + 1)
+
     return cost
 
 
@@ -239,30 +265,60 @@ def pixel_matching(img_left, img_right):
     for d in range(min_disparity, max_disparity):
         M = np.array([[1, 0, d], [0, 1, 0]], dtype='float64')
         img_right_shifted = cv.warpAffine(img_right, M, (img_right.shape[1], img_right.shape[0]))
-        cost = np.sum(img_left - img_right_shifted, axis=2)
+        cost = np.sum(np.abs(img_left - img_right_shifted), axis=2)
         costs.append(cost)
-    disparity = np.argmin(np.array(costs), axis=0)
-    return disparity
+    pixel_disparity = np.argmin(np.array(costs), axis=0)
+
+    # 2. convert estimated disparity to depth, save it to file
+    pixel_depth = disparity_to_depth(pixel_disparity, baseline)
+    write_depth_to_file(pixel_depth, 'pixelDisparity.jpg')
+
+    return pixel_depth
 
 
-def window_matching(img_left, img_right, method):
+def window_matching(img_left, img_right, sim_metric):
     left_patches = extract_pathches(img_left, patch_width)
     # right_patches = extract_pathches(img_right, patch_width)
-    costs = []
+    costs = np.empty((img_left.shape[0], img_left.shape[1], 1))
     for d in range(min_disparity, max_disparity):
         # shift the right_patches matrix by d pixels
         M = np.array([[1, 0, d], [0, 1, 0]], dtype='float64')
         img_right_shifted = cv.warpAffine(img_right, M, (img_right.shape[1], img_right.shape[0]))
+        # img_right_shifted_50px = cv.warpAffine(img_right,
+        #                                        np.array([[1, 0, -50], [0, 1, 0]], dtype='float64'),
+        #                                        (img_right.shape[1], img_right.shape[0]))
+        # cv.imshow('right shifted image', img_right_shifted_50px)
+        # cv.waitKey(0)
+        # cv.destroyAllWindows()
         #     shifted_right_patches --> will be of shape [H, W, feat_size]
         #     left_patches --> will be of shape [H, W, feat_size]
         shifted_right_patches = extract_pathches(img_right_shifted, patch_width)
-        cost = method(left_patches, shifted_right_patches)
-        costs.append(cost)
-    if method is ncc:
-        disparity = np.argmax(np.array(costs), axis=0)
+
+        if sim_metric == 'ssd':
+            cost = ssd(left_patches, shifted_right_patches)
+        else:
+            cost = ncc(left_patches, shifted_right_patches)
+
+        costs = np.append(costs, np.expand_dims(cost, axis=2), axis=2)
+
+    costs = np.array(costs[:, :, 1:])
+
+    costs = mask_outliers(costs, similarity_metric, outlier_threshold)
+
+    if sim_metric == 'ncc':
+        disparity = np.argmax(costs, axis=2)
     else:
-        disparity = np.argmin(np.array(costs), axis=0)
-    return disparity
+        disparity = np.argmin(costs, axis=2)
+
+    # 2. convert estimated disparity to depth, save it to file
+    depth = disparity_to_depth(disparity, baseline)
+    # depth = np.where(depth < np.max(depth) - 1, depth, depth * 0)
+
+    file_name = f'{dataset}_{sim_metric}_N_{outlier_threshold}_Disparity{patch_width}x{patch_width}.jpg'
+    write_depth_to_file(depth, file_name)
+    print(f'{file_name} has been saved!')
+
+    return depth
 
 
 def stereo_matching(img_left, img_right, patch_width):
@@ -272,18 +328,15 @@ def stereo_matching(img_left, img_right, patch_width):
     # This is the main function for your implementation
     # make sure you do the following tasks
     # 1. estimate disparity for every pixel in the left image
-    disparity = pixel_matching(img_left, img_right)
-    disparity = window_matching(img_left, img_right, ncc)
-    # disparity = window_matching(img_left, img_right, ssd)
-
-    # 2. convert estimated disparity to depth, save it to file
-    depth = disparity_to_depth(disparity, baseline)
-    write_depth_to_file(depth, 'pixelDisparity.jpg')
+    depth = pixel_matching(img_left, img_right)
+    # depth = window_matching(img_left, img_right, similarity_metric)
 
     # 3. convert depth to 3D points and save it as colored point cloud using the ply_creator function
     points_3d = depth_to_3d(depth, kmat)
     points_3d_flatten = points_3d.reshape((points_3d.shape[0] * points_3d.shape[1], 3))
-    ply_creator(points_3d_flatten)
+
+    colors = img_left.reshape((points_3d.shape[0] * points_3d.shape[1], 3))
+    ply_creator(points_3d_flatten, colors)
     # 4. visualize the estimted 3D point cloud using meshlab
 
 
